321e81906ff7fa7d579301494db5553327a63ff5
Jason897571 <king.gs1314@outlook.com>
2024-03-04 20:58:21 -0500

    set up manager and employee routers

---
+++ controllers/employeeRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+router.get('/manage-option', async(req, res) => {
+    res.render('manageOption');
+})
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,23 +1,37 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
 
-// main page router
-router.get('/', async (req, res) => {
+// main page router, shows default category
+router.get('/', withAuth, async (req, res) => {
   try {
-    //TODO get post data here and render
+    //TODO get category data here and render
     res.render('homepage');
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+
+// login page
 router.get('/login', async(req, res) => {
   res.render('login');
 })
 
+// sign up page
 router.get('/signup', async(req, res) => {
   res.render('signup');
 })
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;

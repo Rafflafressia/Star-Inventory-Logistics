29ecbbb7dbbfad9f6c9887c044df7dc7c87e63ac
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-03-05 23:24:45 -0500

    Uncommented blocks of code for Product Route

--- controllers/api/productRoute.js
+++ controllers/api/productRoute.js
@@ -17,87 +17,87 @@ router.get('/', async (req, res) => {
     }
 });
 
-// // Get one product
-// router.get('/:id', async (req, res)=>{
-//     try {
-//         const productData = await Product.findByPk(req.params.id,{
-//             include: {model: Category}
-//         })
+// Get one product
+router.get('/:id', async (req, res)=>{
+    try {
+        const productData = await Product.findByPk(req.params.id,{
+            include: {model: Category}
+        })
 
-//         if (!productData){
-//             res.status(404).json({message: "Product could not be found"});
-//         }
+        if (!productData){
+            res.status(404).json({message: "Product could not be found"});
+        }
 
-//         res.status(200).json(productData);
-//     } catch(err) {
-//         res.status(404).json({message:"This product does not exist"});
-//     }
-// });
+        res.status(200).json(productData);
+    } catch(err) {
+        res.status(404).json({message:"This product does not exist"});
+    }
+});
 
 
-// // Create a new product
-// router.post('/', async (req, res) => {
-//     /*
-//         {
-//             product_name:
-//             price:
-//             cost:
-//             stock:
-//             description:
-//             category_id:
-//         }
-//     */
-//     try {
-//         const productData = await Product.create(req.body);
+// Create a new product
+router.post('/', async (req, res) => {
+    /*
+        {
+            product_name:
+            price:
+            cost:
+            stock:
+            description:
+            category_id:
+        }
+    */
+    try {
+        const productData = await Product.create(req.body);
 
-//         res.status(200).json(productData);
+        res.status(200).json(productData);
 
-//     } catch(err){
+    } catch(err){
 
-//         res.status(400).json({message: "Could not create new post"});
+        res.status(400).json({message: "Could not create new post"});
 
-//     }
-// });
+    }
+});
 
-// router.put("/:id", async (req, res)=>{
+router.put("/:id", async (req, res)=>{
     
-//     try {
-//         const productData = await Product.update(req.body, {
-//             where: {id: req.params.id},
-//         });
+    try {
+        const productData = await Product.update(req.body, {
+            where: {id: req.params.id},
+        });
 
-//         if (!productData[0]) {
-//             res.status(404).json({message: 'No product found'});
-//         }
+        if (!productData[0]) {
+            res.status(404).json({message: 'No product found'});
+        }
 
-//         res.status(200).json(productData);
-//     } catch(err) {
-//         res.status(500).json(err);
-//     }
-// });
+        res.status(200).json(productData);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
 
-// router.delete('/:id', async (req, res)=> {
-//     try {
+router.delete('/:id', async (req, res)=> {
+    try {
 
-//         const productData = await Product.findByPk(req.params.id);
+        const productData = await Product.findByPk(req.params.id);
 
-//         if (!productData) {
+        if (!productData) {
 
-//             return res.status(404).json({message: "Product could not be found"});
+            return res.status(404).json({message: "Product could not be found"});
 
-//         }
+        }
 
-//         await Product.destroy();
+        await Product.destroy();
 
-//         res.status(200).json({message: 'Product has been deleted successfully'});
+        res.status(200).json({message: 'Product has been deleted successfully'});
 
-//     } catch (err){
+    } catch (err){
 
-//         console.error('Error deleting product: ' , err);
+        console.error('Error deleting product: ' , err);
 
-//         res.status(500).json({error: 'Internal server error'});
+        res.status(500).json({error: 'Internal server error'});
 
-//     }
-// });
+    }
+});
 
 module.exports = router; 

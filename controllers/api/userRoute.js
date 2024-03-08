4d08e344b62f871cfb8cc9c1532ea75ebd39f83e
Jason897571 <king.gs1314@outlook.com>
2024-03-07 20:54:10 -0500

    SIL-53 fix bug when login

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -5,43 +5,43 @@ const {UserData, Employee} = require('../../models');
 
 router.post('/login', async (req, res) => {
 
-    try{
-        const userData = await UserData.findOne({
-            where:{user_name: req.body.username}
-        })
-
-    
-        if(!userData){
-            res.status(400).json({message: 'Incorrect username or password, please try again'});
-            return;
-        }
-        
-        /* Check if the password is correct */
-        const validPassword = await userData.checkPassword(req.body.password);
+  try{
+      const userData = await UserData.findOne({
+          where:{user_name: req.body.user_name}
+      })
 
   
-        if(!validPassword){
-            res.status(400).json({message: 'Incorrect username or password, please try again'});
-        
-            return;
-        }
+      if(!userData){
+          res.status(400).json({message: 'Incorrect username or password, please try again'});
+          return;
+      }
+      
+      /* Check if the password is correct */
+      const validPassword = await userData.checkPassword(req.body.password);
 
-        const position = await Employee.findOne({
-            where: {user_id: userData.dataValues.user_id}
-        })
-        
-        /* save session info */
-        req.session.save(() => {
-            req.session.user_id = userData.dataValues.user_id;
-            req.session.logged_in = true;
-            req.session.position = position.dataValues.position;
-    
-            res.json({ user: userData, message: 'You are now logged in!' });
-        })
-    }
-    catch(err){
-        res.status(500).json({"message":err});
-    }
+
+      if(!validPassword){
+          res.status(400).json({message: 'Incorrect username or password, please try again'});
+      
+          return;
+      }
+
+      const position = await Employee.findOne({
+          where: {user_id: userData.dataValues.user_id}
+      })
+
+      /* save session info */
+      req.session.save(() => {
+          req.session.user_id = userData.dataValues.user_id;
+          req.session.logged_in = true;
+          req.session.position = position.dataValues.position;
+  
+          res.json({ user: userData, message: 'You are now logged in!' });
+      })
+  }
+  catch(err){
+      res.status(500).json({"message":err});
+  }
    
 
 })

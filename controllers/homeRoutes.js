e9b9fcf759e32aacec60ee1ede48f84acc3300cd
Jason897571 <king.gs1314@outlook.com>
2024-03-07 21:57:11 -0500

    SIL-22 add position as a pram to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,7 +7,7 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     //TODO get category data here and render
-    res.render('homePage');
+    res.render('homePage',{position: req.session.position});
   } catch (err) {
     res.status(500).json(err);
   }
@@ -16,13 +16,17 @@ router.get('/', async (req, res) => {
 
 // login page
 router.get('/login', async(req, res) => {
-  
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
   res.render('login');
+
 })
 
 
 // show product details
-router.get('/product-detail', async(req, res) => {
+router.get('/product-detail', withAuth, async(req, res) => {
   res.render('productDetail');
 })
 

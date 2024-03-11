e71e78e9ba1cd39d43636a4296bba6a8f0eec064
Jason897571 <king.gs1314@outlook.com>
2024-03-10 21:55:15 -0400

    add modal for add employee page

--- public/js/addEmployee.js
+++ public/js/addEmployee.js
@@ -5,6 +5,11 @@ const add_new_employee_handler = async(event) => {
     const user_name = document.querySelector('#username').value.trim();
     const password = document.querySelector('#password').value.trim();
     const position = document.querySelector('#position').value.trim();
+
+    // Medal
+    const status_title = document.querySelector('#exampleModalToggleLabel');
+    const message = document.querySelector('.modal-body');
+    const pop_up = document.querySelector('.pop-up');
     
     if(first_name && last_name && user_name && password && position){
         const response = await fetch('/api/add-employee', {
@@ -20,12 +25,20 @@ const add_new_employee_handler = async(event) => {
         })
 
         if (response.ok){
-           
-            console.log("New employee added successfully");
+           status_title.innerText = "Empolyee created Successfully";
+           message.innerText = "New employee has been added successfully";
+           pop_up.click();
+        
 
         }else{
-            alert("Failed to create a employee");
+           status_title.innerText = "Empolyee created failed";
+           message.innerText = "Please check your inputs and try again";
+           pop_up.click();
         }
+    }else{
+        status_title.innerText = "Empolyee created failed";
+        message.innerText = "Please fill all boxes";
+        pop_up.click();
     }
 }
 

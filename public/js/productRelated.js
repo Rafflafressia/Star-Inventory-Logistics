6e583d281ddf72172e5c25d32d6dc69c16016bce
Jason897571 <king.gs1314@outlook.com>
2024-03-12 00:31:14 -0400

    SIL-80 working on product related page

--- public/js/productRelated.js
+++ public/js/productRelated.js
@@ -1,7 +1,16 @@
 const add_new_product_handler = () => {
     document.location.replace('/add-product');
 }
+const search_handler = async () => {
 
+    const search_bar = document.querySelector('#product-search-text');
+    const product_name = search_bar.value.trim();
+  
+    document.location.assign(`/search-result?product_name=${encodeURIComponent(product_name)}`);
+  
+  }
+  
+  document.querySelector('.ss').addEventListener('click', search_handler);
   
 
 
